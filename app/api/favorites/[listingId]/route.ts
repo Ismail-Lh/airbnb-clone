e775135e5c4bfc9978a787b0610a9796f9bd6101
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-12 17:47:33 +0000

    feat(API): create the backend functionality for adding favorites listing

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,33 @@
+/* eslint-disable import/prefer-default-export */
+import { NextResponse } from 'next/server';
+
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import prisma from '@/app/libs/prismadb';
+
+interface IParams {
+  listingId?: string;
+}
+
+export async function POST(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string')
+    throw new Error('Invalid ID!');
+
+  const favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds.push(listingId);
+
+  const updatedUser = await prisma.user.update({
+    where: { id: currentUser.id },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
+}
