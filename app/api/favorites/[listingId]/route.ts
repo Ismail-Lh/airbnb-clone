ba08356ba90269aa9c6b40436773d60a3ae9d86d
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-13 14:52:00 +0000

    feat(API): create the backend api route for remove favorite listings

--- app/api/favorites/[listingId]/route.ts
+++ app/api/favorites/[listingId]/route.ts
@@ -31,3 +31,27 @@ export async function POST(req: Request, { params }: { params: IParams }) {
 
   return NextResponse.json(updatedUser);
 }
+
+export async function DELETE(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const { listingId } = params;
+
+  if (!listingId || typeof listingId !== 'string')
+    throw new Error('Invalid ID!');
+
+  let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+  favoriteIds = favoriteIds.filter((id) => id !== listingId);
+
+  const updatedUser = await prisma.user.update({
+    where: { id: currentUser.id },
+    data: {
+      favoriteIds,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
+}
