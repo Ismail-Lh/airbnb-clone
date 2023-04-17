af28115c1242bae6bb89876f131fc79aacd47fd8
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-17 17:34:51 +0000

    feat(API): create the backen functionality for the reservations route

---
+++ app/api/reservations/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import prisma from '@/app/libs/prismadb';
+
+export async function POST(req: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const { listingId, startDate, endDate, totalPrice } = await req.json();
+
+  if (!listingId || !startDate || !endDate || !totalPrice)
+    return NextResponse.error();
+
+  const listingAndReservation = await prisma.listing.update({
+    where: { id: listingId },
+    data: {
+      reservations: {
+        create: {
+          userId: currentUser.id,
+          startDate,
+          endDate,
+          totalPrice,
+        },
+      },
+    },
+  });
+
+  return NextResponse.json(listingAndReservation);
+}
+
+export async function GET(request: Request) {
+  return request;
+}
