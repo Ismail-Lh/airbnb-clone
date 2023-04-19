ae80a6a44b5e7b3ab35a178dd62b8715885af5ec
Ismail-Lh <ismailsaad03@gmail.com>
2023-04-19 16:29:45 +0000

    feat(API): create the cancel reservation backend functionality

---
+++ app/api/reservations/[reservationId]/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+
+import getCurrentUser from '@/app/actions/getCurrentUser';
+import prisma from '@/app/libs/prismadb';
+
+interface IParams {
+  reservationId?: string;
+}
+
+export async function DELETE(req: Request, { params }: { params: IParams }) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) return NextResponse.error();
+
+  const { reservationId } = params;
+
+  if (!reservationId || typeof reservationId !== 'string')
+    throw new Error('Invalid ID!');
+
+  const reservation = await prisma.reservation.deleteMany({
+    where: {
+      id: reservationId,
+      OR: [{ userId: currentUser.id }, { listing: { userId: currentUser.id } }],
+    },
+  });
+
+  return NextResponse.json(reservation);
+}
+
+export async function GET(request: Request) {
+  return request;
+}
